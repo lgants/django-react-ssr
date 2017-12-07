ab4a330059801163ff41adaf595734544e6ca7d6
lgants <lgants@gmail.com>
2017-12-06 22:52:57 -0600

    updated views

--- backend/app/views.py
+++ backend/app/views.py
@@ -2,5 +2,18 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from react.render import render_component
 
-# Create your views here.
+comments = []
+
+def index():
+    rendered = render_component(
+        os.path.join(os.getcwd(), 'static', 'js', 'CommentBox.jsx'),
+        {
+            'comments': comments,
+            'url': '/comment/',
+        },
+        to_static_markup=True,
+    )
+
+return render_template('index.html', rendered=rendered)
