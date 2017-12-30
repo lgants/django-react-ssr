cf7961747e8af656910dcd8b1e72c8bd529e7a94
lgants <lgants@gmail.com>
2017-12-30 15:20:01 -0600

    deprecated livereload install and updated webpack

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -40,7 +40,6 @@ DEFAULT_APPS = [
 
 THIRD_PARTY_APPS = [
     'react',
-    'livereload',
     'webpack_loader',
 ]
 
@@ -63,7 +62,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'livereload.middleware.LiveReloadScript',
 ]
 
 ROOT_URLCONF = 'project.urls'
